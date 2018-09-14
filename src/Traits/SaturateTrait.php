11d787b7a9c08e278acd422941d3a5ad9bd308f0
Wolfy-J <wolfy.jd@gmail.com>
2018-09-14 14:49:15 +0300

    saturate trait added

---
+++ src/Traits/SaturateTrait.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Core\Traits;
+
+use Psr\Container\ContainerExceptionInterface;
+use Spiral\Core\ContainerScope;
+use Spiral\Core\Exceptions\ScopeException;
+
+/**
+ * Saturate optional constructor or method argument (class) using internal (usually static)
+ * container. In most of cases trait is doing nothing since spiral Container populates even
+ * optional class dependencies.
+ *
+ * Avoid using this trait in custom code, it's only a development sugar.
+ */
+trait SaturateTrait
+{
+    /**
+     * Must be used only to resolve optional constructor arguments. Use in classes which are
+     * generally resolved using Container. Default value MUST always be supplied from outside.
+     *
+     * @param mixed  $default Default value.
+     * @param string $class   Requested class.
+     *
+     * @return mixed
+     *
+     * @throws ScopeException
+     */
+    private function saturate($default, string $class)
+    {
+        if (!empty($default)) {
+            return $default;
+        }
+
+        $container = ContainerScope::getContainer();
+        if (empty($container)) {
+            throw new ScopeException("Unable to saturate '{$class}': no container available");
+        }
+
+        try {
+            return $container->get($class);
+        } catch (ContainerExceptionInterface $e) {
+            throw new ScopeException("Unable to saturate '{$class}': {$e->getMessage()}", 0, $e);
+        }
+    }
+}
