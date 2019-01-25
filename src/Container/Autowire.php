b57767328c3411d64ba496b4d8c8b189c6e512b2
Wolfy-J <wolfy.jd@gmail.com>
2019-01-25 16:43:16 +0300

    strict types

--- src/Container/Autowire.php
+++ src/Container/Autowire.php
@@ -1,8 +1,10 @@
 <?php
+declare(strict_types=1);
 /**
- * components
+ * Spiral Framework.
  *
- * @author    Wolfy-J
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
  */
 
 namespace Spiral\Core\Container;
