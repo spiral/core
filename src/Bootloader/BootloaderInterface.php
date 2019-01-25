9fa1dcad94c961be2d8d09e038353d6c22b9d7ab
Wolfy-J <wolfy.jd@gmail.com>
2019-01-25 16:48:28 +0300

    strict types

--- src/Bootloader/BootloaderInterface.php
+++ src/Bootloader/BootloaderInterface.php
@@ -1,10 +1,12 @@
 <?php
+declare(strict_types=1);
 /**
  * Spiral Framework.
  *
  * @license   MIT
  * @author    Anton Titov (Wolfy-J)
  */
+
 namespace Spiral\Core\Bootloader;
 
 /**
--- src/Traits/Config/AliasTrait.php
+++ src/Traits/Config/AliasTrait.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /**
  * Spiral Framework.
  *
@@ -15,7 +16,6 @@ trait AliasTrait
 {
     /**
      * @param string $alias
-
      * @return string
      */
     public function resolveAlias(string $alias): string
